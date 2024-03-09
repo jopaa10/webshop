d2a18de15e75e8c2a8dcf3c034d25d8d222b7c46
Josipa Znaor <josipa.znaor@tildeloop.com>
2024-03-10 00:49:10 +0100

    Keep calm and commit

--- src/features/user/userSlice.tsx
+++ src/features/user/userSlice.tsx
@@ -39,7 +39,7 @@ const userSlice = createSlice({
     logoutUser: (state) => {
       state.user = null;
       localStorage.removeItem("user");
-      console.log("logout njsdks");
+      console.log("logout");
     },
   },
 });
