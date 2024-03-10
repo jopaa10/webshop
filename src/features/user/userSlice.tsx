ef203f34485bf87dbfda07839c3230e6af687f7e
Josipa Znaor <josipa.znaor@tildeloop.com>
2024-03-10 01:12:05 +0100

    test

--- src/features/user/userSlice.tsx
+++ src/features/user/userSlice.tsx
@@ -39,7 +39,7 @@ const userSlice = createSlice({
     logoutUser: (state) => {
       state.user = null;
       localStorage.removeItem("user");
-      console.log("logout dsjnfsjdk dsnf");
+      console.log("logout dsjnfsjdk");
     },
   },
 });
